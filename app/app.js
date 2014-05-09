b65403ecc278c6cb586b4a981e509c1ed3318a88
Chris Slater <chris.slater@ratedpeople.com>
2014-05-09 14:17:05 +0100

    Add App level listener to application

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 define(['backbone', 'jquery'],function(Backbone, $){
   var App = {};
+
+  // Add global event handler to App.
+  _.extend(App, Backbone.Events);
+
   App.start = function(){
     console.log('App is ready to start');
   };
